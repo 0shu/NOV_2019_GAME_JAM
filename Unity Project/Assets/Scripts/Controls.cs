146a3cb6dd0687a1147ad07914b3dbfa6b2b7ffc
Charlie Uni Computer <48212893+CharlieJones0919@users.noreply.github.com>
2019-11-16 18:42:18 +0000

    Sliding Movement Implemented

--- Unity Project/Assets/Scripts/Controls.cs
+++ Unity Project/Assets/Scripts/Controls.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class Controls : MonoBehaviour
 {
+    //Player Variables
     Rigidbody2D player;
     float acceleration = 0.25f;
     float maxVelocity = 10;
 
+    //Lane Variables
     float[] laneHeight = {-5, 0, 5};
     uint currentLane;
 
-    float maxJumpHeight = 1.75f;
-    float jumpVelocity = 4;
+    //Jump Variables
+    float maxJumpHeight = 3f;
+    float jumpandFallAcc = 7.5f;
     bool jumping = false;
-    float jumpandFallAcc = 3.5f;
 
-    float obsticalPushBack = 5f;
+    bool sliding = false;
+
+    bool gotSpeedBoost = false;
+    bool gotSpeedSteal = false;
+
+    //Obstacle Variables
+    float obstaclePushBack = 15f;
+    float speedBoost = 2.5f;
+    float speedSteal = -1f;
 
     void Start()
     {
@@ -29,47 +39,63 @@ public class Controls : MonoBehaviour
 
     void Update()
     {
-        PlayerMovement();
+        //Check for Action
+        ChangeLaneCheck();
+        CheckMovement();
+        CheckJump();
+        CheckSlide();
 
+       if ((gotSpeedBoost == false) && (player.velocity.x > maxVelocity))
+       {
+            player.velocity = new Vector2(player.velocity.x - (acceleration / 20), player.velocity.y);
+       }
+
+        Debug.Log(player.velocity);
     }
 
-    void PlayerMovement()
+    void ChangeLaneCheck()
     {
         //Move Up Lane
         if ((Input.GetKeyDown(KeyCode.W)) && (currentLane < 2))
         {
             currentLane++;
             player.position = new Vector2(player.position.x, laneHeight[currentLane]);
-        }  
+        }
         //Move Down Lane
         else if ((Input.GetKeyDown(KeyCode.S)) && (currentLane > 0))
         {
             currentLane--;
             player.position = new Vector2(player.position.x, laneHeight[currentLane]);
         }
+    }
 
+    void CheckMovement()
+    {
         //Move Left
-        if ((Input.GetKey(KeyCode.A) && (player.velocity.x > -maxVelocity)))
+        if (Input.GetKey(KeyCode.A) && (player.velocity.x > -maxVelocity))
         {
             player.velocity = new Vector2(player.velocity.x - acceleration, player.velocity.y);
         }
         //Decellerating
         else if (player.velocity.x < 0)
         {
-            player.velocity = new Vector2(player.velocity.x + (acceleration/2), player.velocity.y);
+            player.velocity = new Vector2(player.velocity.x + (acceleration / 2), player.velocity.y);
         }
 
         //Move Right
-        if ((Input.GetKey(KeyCode.D)) && (player.velocity.x < maxVelocity))
+        if (Input.GetKey(KeyCode.D) && (player.velocity.x < maxVelocity))
         {
             player.velocity = new Vector2(player.velocity.x + acceleration, player.velocity.y);
         }
         //Decellerating
         else if (player.velocity.x > 0)
         {
-            player.velocity = new Vector2(player.velocity.x - (acceleration/2), player.velocity.y);
+            player.velocity = new Vector2(player.velocity.x - (acceleration / 2), player.velocity.y);
         }
+    }
 
+    void CheckJump()
+    {
         //Jump
         if (Input.GetKey(KeyCode.Space) && (jumping == false))
         {
@@ -84,11 +110,13 @@ public class Controls : MonoBehaviour
         {
             jumping = true;
         }
-      
+
+        //Falling Back Down
         if ((player.position.y > laneHeight[currentLane]) && (jumping == true))
         {
             player.velocity = new Vector2(player.velocity.x, -jumpandFallAcc);
         }
+        //Reallignment with Lane
         else if (player.position.y <= laneHeight[currentLane])
         {
             player.position = new Vector2(player.position.x, laneHeight[currentLane]);
@@ -96,11 +124,58 @@ public class Controls : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter(Collision obstical)
+    void CheckSlide()
     {
-        if (obstical.gameObject.tag == "StoppingObst")
+        //Slide
+        if (Input.GetKey(KeyCode.LeftShift))
         {
-            player.velocity = new Vector2(player.velocity.x - obsticalPushBack, player.velocity.y);
+            if (player.velocity.x > 0)
+            {
+                player.velocity = new Vector2(player.velocity.x - (acceleration / 10), player.velocity.y);
+            }
+            else if (player.velocity.x < 0)
+            {
+                player.velocity = new Vector2(player.velocity.x + (acceleration / 10), player.velocity.y);
+            }
+            sliding = true;
+        }
+        else
+        {
+            sliding = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D obstacle)
+    {
+        if (obstacle.gameObject.tag == "StoppingObst")
+        {
+            player.velocity = new Vector2(player.velocity.x - obstaclePushBack, player.velocity.y);
+        }
+        else if (obstacle.gameObject.tag == "SlidingObst")
+        {
+            if (sliding == true)
+            {
+                gotSpeedBoost = true;
+            }
+            else
+            {
+                gotSpeedSteal = true;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D obstacle)
+    {
+        if (gotSpeedBoost == true)
+        {
+            player.velocity = new Vector2(player.velocity.x + (player.velocity.x * speedBoost), player.velocity.y);
+            gotSpeedBoost = false;
+        }
+
+        if (gotSpeedSteal == true)
+        {
+            player.velocity = new Vector2(player.velocity.x + (player.velocity.x * speedSteal), player.velocity.y);
+            gotSpeedSteal = false;
         }
     }
 }
