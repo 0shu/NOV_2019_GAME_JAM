bc4902f2deb228b43f71b4eec200496c3caecf8d
Charlie Uni Computer <48212893+CharlieJones0919@users.noreply.github.com>
2019-11-16 15:47:20 +0000

    Rebuilt Control Script with Consideration to Lanes (Attempt 1)

--- Unity Project/Assets/Scripts/Controls.cs
+++ Unity Project/Assets/Scripts/Controls.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class Controls : MonoBehaviour
 {
     Rigidbody2D player;
-    float speedIncreaser = 0.5f;
+    float acceleration = 0.5f;
     float maxVelocity = 2;
 
-    GameObject ground;
-    float maxJumpVel = 2;
+    float[] laneheight = {0, 10, 20};
+    uint currentLane;
+
+    float maxJumpVel = 2.0f;
+    float jumpVelocity = 2;
+    bool jumped = false;
+
+    float obsticalPushBack = 5f;
 
     void Start()
     {
         player = gameObject.GetComponent<Rigidbody2D>();
         player.velocity = new Vector2(0, 0);
 
-        ground = GameObject.FindGameObjectWithTag("Ground");
+        currentLane = 1;
+        player.position = new Vector2(player.position.x, laneheight[currentLane]);
     }
 
     void Update()
@@ -26,45 +33,60 @@ public class Controls : MonoBehaviour
 
     void PlayerMovement()
     {
-        if ((Input.GetKey(KeyCode.RightArrow)) && (player.velocity.x < maxVelocity))
+        //Move Up Lane
+        if ((Input.GetKeyDown(KeyCode.W)) && (currentLane < 2))
         {
-            player.velocity = new Vector2(player.velocity.x + speedIncreaser, player.velocity.y);
-        }
-        else if (player.velocity.x != 0)
+            currentLane++;
+            player.position = new Vector2(player.position.x, laneheight[currentLane + 1]);
+        }  
+        //Move Down Lane
+        else if ((Input.GetKeyDown(KeyCode.S)) && (currentLane > 0))
         {
-            player.velocity = new Vector2(player.velocity.x - speedIncreaser, player.velocity.y);
-        }
-
-        if ((Input.GetKey(KeyCode.LeftArrow) && (player.velocity.x > -maxVelocity)))
-        {
-            player.velocity = new Vector2(player.velocity.x - speedIncreaser, player.velocity.y);
-        }
-        else if (player.velocity.x != 0)
-        {
-            player.velocity = new Vector2(player.velocity.x + speedIncreaser, player.velocity.y);
+            currentLane--;
+            player.position = new Vector2(player.position.x, laneheight[currentLane - 1]);
         }
 
-        if ((Input.GetKey(KeyCode.UpArrow)) || (Input.GetKey(KeyCode.W) && (GroundedCheck() == true)))
+        //Move Left
+        if ((Input.GetKey(KeyCode.A) && (player.velocity.x > -maxVelocity)))
         {
-            player.velocity = new Vector2(player.velocity.x, player.velocity.y + maxJumpVel);
+            player.velocity = new Vector2(player.velocity.x - acceleration, player.velocity.y);
+        }
+        //Decellerating
+        else if (player.velocity.x <= 0)
+        {
+            player.velocity = new Vector2(player.velocity.x + acceleration, player.velocity.y);
+        }
+        //Move Right
+        if ((Input.GetKey(KeyCode.D)) && (player.velocity.x < maxVelocity))
+        {
+            player.velocity = new Vector2(player.velocity.x + acceleration, player.velocity.y);
+        }
+        //Decellerating
+        else if (player.velocity.x >= 0)
+        {
+            player.velocity = new Vector2(player.velocity.x - acceleration, player.velocity.y);
+        }
+
+        //Jump
+        if ((Input.GetKeyDown(KeyCode.Space) && (player.position.x > -maxVelocity)) && (jumped == false))
+        {
+            player.velocity = new Vector2(player.velocity.x, jumpVelocity);
+        }
+        else if (player.position.y > laneheight[currentLane])
+        {
+            player.velocity = new Vector2(player.velocity.x, -acceleration);
+        }
+        else if (player.position.y < laneheight[currentLane])
+        {
+            player.position = new Vector2(player.position.x, laneheight[currentLane]);
         }
     }
 
-    bool GroundedCheck()
+    void OnCollisionEnter(Collision obstical)
     {
-        float distToGround;
-        bool grounded;
-        distToGround = Vector2.Distance(ground.transform.position, transform.position);
-
-        if (distToGround < 0.01f)
+        if (obstical.gameObject.tag == "StoppingObst")
         {
-            grounded = true;
+            player.velocity = new Vector2(player.velocity.x - obsticalPushBack, player.velocity.y);
         }
-        else
-        {
-            grounded = false;
-        }
-
-        return grounded;
     }
 }
